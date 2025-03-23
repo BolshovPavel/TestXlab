476c4a4c89d3e2a6164835bb3ce231a7ae81b7e3
BolshovPavel <Bolshov23@yandex.ru>
2025-03-23 22:50:30 +0300

    Unit 2_1

--- TestUnit2/TestProgram/Program.cs
+++ TestUnit2/TestProgram/Program.cs
@@ -7,9 +7,30 @@ using System.Threading.Tasks;
 namespace TestProgram
 {
     internal class Program
+
+        static void Main (string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+        int a = 3;
+        int b;
+        b = 4;
+
+        byte b;
+        sbyte sb;
+        short s;
+        ushort us;
+        int i;
+        uint ui;
+        long l;
+        ulong ul;
+
+        float f = 6.2f;
+        double d = 5.7;
+
+        char c = '5';
+
+        string str = "hello there";
+
+        bool bl;
     }
+    
 }
